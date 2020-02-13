e37ba5d754219d71d94fdae32302207544908bba
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2020-02-13 13:05:28 -0500

    add JSON Decoder logic and related unit test

--- Quakes/QuakesTests/QuakeTests.swift
+++ Quakes/QuakesTests/QuakeTests.swift
@@ -11,9 +11,28 @@ import XCTest
 
 class QuakeTests: XCTestCase {
 
+    //
     // Use throws so you don't need do/catch
+    
+    
     func testQuake() throws {
+        //
+        // Test Driven Developer - TDD
+        // Red - Failing Test
+        // Green - Passing Test
+        // Refactor - Cleanup your mess
         
+        let decoder = JSONDecoder()
+        
+        let quake = try decoder.decode(Quake.self, from: quakeData)
+        
+        // Assert (Expected, actual)
+        XCTAssertEqual(1.29, quake.magnitude, accuracy: 0.00001)
+        XCTAssertEqual("10km SSW of Idyllwild, CA", quake.place)
+//        XCTAssertEqual(date, quake.time)
+        XCTAssertEqual(-116.7776667, quake.longitude, accuracy: 0.001)
+        XCTAssertEqual(33.663333299999998, quake.latitude, accuracy: 0.001)
+
     }
 
 }
